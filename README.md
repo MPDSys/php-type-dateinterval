056578d37495cf1d1188c86670857d1f8e314c72
YuxiUx <abcad@post.cz>
2025-01-02 18:58:55 +0100

    Add comparators

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ with php DateInterval
 ## Usage
 ```php
 require_once 'vendor/autoload.php';
-
 use mpd\Type\DateInterval\DateInterval;
 
 $i = new DateInterval("P2Y3M1DT8H4M3S");
@@ -22,7 +21,18 @@ $i = DateInterval::createFromDateString("+2 days");
 echo($i->toDuration() . "\n"); //"P2D"
 
 // see https://www.php.net/manual/en/dateinterval.format.php
-$i->format("%d days"); //"2 days"
+echo($i->format("%d days") . "\n"); //"2 days"
+
+$b = new DateInterval("PT48H");
+$c = new DateInterval("P2D");
+echo($b->isExactlyEqual($i)); // false
+echo($c->isExactlyEqual($i)); // true
+
+$date = new DateTime("1970-01-01 00:01:00");
+echo($b->cmpRelative($c, $date)); // 0 (equal)
+$i = new DateInterval("P2DT1S");
+echo($c->cmpRelative($i, $date)); // -1 (c < i)
+echo($i->cmpRelative($c, $date)); //  1 (i > c
 ```
 
 
@@ -44,7 +54,28 @@ class DateInterval {
     public __construct(string $duration)
     public toDuration(): string
     public format(string $format): string
+    public cmpRelative(\DateInterval $b, ?DateTimeInterface $baseDate = null): int
+    public isExactlyEqual(\DateInterval $b): bool 
     public static convertToDurationString(\DateInterval $i): string 
     public static createFromDateString(string $datetime): DateInterval|false
+    public static compareRelativeToDate(\DateInterval $a, \DateInterval $b, ?DateTimeInterface $base=null): int
+    public static areExactlyEqual(\DateInterval $a, \DateInterval $b): bool
 }
 ```
+
+## Notes
+This class is based on php's native DateInterval class, can be used as drop in
+replacement. but also inherits some recommendations like 
+"properties should be considered read only". 
+
+### isExactlyEqual / areExactlyEqual
+Compare intervals by components. 24H != 1D, 24H == 24H, 1D == 1D
+
+### cmpRelative / compareRelativeToDate
+Intervals cant be easily compared. For reasons like  
+1Y == 365,366D;  1M == 28,29,30,31D;  leap seconds    
+But you can compare them exactly from given starting point. 
+You can either specify your own or use implicit current time
+which gives accurate enough comparison for a lot of cases.  
+But yes, in some cases this still might not be kind of comparison you need.  
+
--- src/DateInterval.php
+++ src/DateInterval.php
@@ -1,6 +1,8 @@
 <?php
 namespace mpd\Type\DateInterval;
 
+use DateTimeInterface;
+
 /**
  * @inheritDoc
  */
@@ -13,6 +15,30 @@ class DateInterval extends \DateInterval
      */
     public function toDuration(): string {
         return self::convertToDurationString($this);
+    }
+
+    /**
+     * Compares interval size relative to given starting date
+     *
+     * @param \DateInterval $b
+     * @param ?DateTimeInterface $baseDate starting point or now when null
+     * @return int -1 if < b | if equals b | 1 when
+     */
+    public function cmpRelative(\DateInterval $b, ?DateTimeInterface $baseDate = null): int {
+        return self::compareRelativeToDate($this, $b, $baseDate);
+    }
+
+    /**
+     * Check if all components are equal.
+     *
+     * Note 24H != 1D. 24H equals only to 24H.
+     * For more benevolent comparison check cmpRelative
+     *
+     * @param \DateInterval $b
+     * @return bool
+     */
+    public function isExactlyEqual(\DateInterval $b): bool {
+        return self::AreExactlyEqual($this, $b);
     }
 
     /**
@@ -45,5 +71,45 @@ class DateInterval extends \DateInterval
 
         if(!empty($t)) $d .= 'T' . $t;
         return $d;
+    }
+
+    /**
+     * Compares two DateIntervals relative to given starting date or now
+     *
+     * @param \DateInterval $a
+     * @param \DateInterval $b
+     * @param ?DateTimeInterface $base Date To calculate from
+     * @return int a<b => -1 | a=b => 0 | a > b => 1
+     */
+    public static function compareRelativeToDate(\DateInterval $a, \DateInterval $b, ?DateTimeInterface $base=null): int {
+        if($base === null)
+            $base = new \DateTimeImmutable();
+        else // ensure immutability
+            $base = \DateTimeImmutable::createFromInterface($base);
+        $da = $base->add($a);
+        $db = $base->add($b);
+        return $da <=> $db;
+    }
+
+    /**
+     * Check for equality of components.
+     *
+     * Note 24H != 1D . 24H equals only 24H and 1D to 1D
+     * if you want 24H to equal 1D use compareRelativeToDate
+     *
+     * @param \DateInterval $a
+     * @param \DateInterval $b
+     * @return bool
+     */
+    public static function areExactlyEqual(\DateInterval $a, \DateInterval $b): bool {
+        return
+            $a->y == $b->y &&
+            $a->m == $b->m &&
+            $a->d == $b->d &&
+            $a->h == $b->h &&
+            $a->i == $b->i &&
+            $a->s == $b->s &&
+            $a->f == $b->f &&
+            $a->invert == $b->invert;
     }
 }
